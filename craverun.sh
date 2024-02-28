9973966d68c18bd50fb9aaf4fd594bf77170b45b
xc112lg <xc112lg@users.noreply.github.com>
2024-02-28 21:49:20 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -9,13 +9,8 @@ crave run --no-patch -- "              #Im not a bot
           #© crave.io Inc. 2017-2024
         #no dual account just to build faster
               #respect the rule
-#-j$(nproc --all)
+#
 
-rm -rf .repo/local_manifests 
-git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
-lunch -userdebug
-m bacon
+-j$(nproc --all)
 "
 . a.sh
