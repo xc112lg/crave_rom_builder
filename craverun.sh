8470678c0068da6b4e2698f86329b7a72126e3e2
xc112lg <xc112lg@users.noreply.github.com>
2024-02-28 21:29:12 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -9,7 +9,13 @@ crave run --no-patch -- "              #Im not a bot
           #© crave.io Inc. 2017-2024
         #no dual account just to build faster
               #respect the rule
-#
-echo -j$(nproc --all)
+#this is only a test do not mind this second queue i cancel later
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+rm -rf .repo/local_manifests 
+git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+source build/envsetup.sh
+lunch lineage_h872-userdebug
+m bacon
 "
 . a.sh
