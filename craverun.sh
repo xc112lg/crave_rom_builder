aa31ff42a7d6908166d430385b6c23bc15365dc0
xc112lg <xc112lg@users.noreply.github.com>
2024-02-28 22:19:27 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,17 +4,17 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch -- "              #Im not a bot
+crave run --no-patch --"              #Im not a bot
           #Thanks to UV n Team
           #© crave.io Inc. 2017-2024
         #no dual account just to build faster
               #respect the rule
-#
+#this is only a test do not mind this second queue i cancel later 
 
 rm -rf .repo/local_manifests 
 git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
               #respect the rule
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j\2 --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 lunch lineage_h872-userdebug
 m bacon
