56b9b8a39a2b130e48140a80c0c70ac9019cab4a
xc112lg <xc112lg@users.noreply.github.com>
2024-02-28 13:06:58 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,17 +4,12 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch --clean -- "              #Im not a bot
+crave run --no-patch -- "              #Im not a bot
           #Thanks to UV n Team
           #© crave.io Inc. 2017-2024
         #no dual account just to build faster
               #respect the rule
-#say something nice, ice for sale
-rm -rf .repo/local_manifests 
-git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
-repo sync -c -j16 --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
-lunch lineage_h872-userdebug
-m bacon
+#might take sometime to build mine sorry guys
+mkdir -p cc &&rm -rf scripts prebuilts&& git clone https://github.com/xc112lg/scripts.git -b cd10 && repo init --depth 1 -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs  &&  chmod u+x scripts/sync.sh &&bash scripts/sync.sh
 "
 . a.sh
--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd LineageOS21
-repo init -u https://github.com/accupara/los21.git -b lineage-21.0 --git-lfs --depth=1
+cd DerpFest13
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 --depth=1
 chmod +x craverun.sh
 . craverun.sh
