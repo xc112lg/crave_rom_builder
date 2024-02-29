f1690e61d8985252a4bb599a4156b04cab10fb1e
xc112lg <xc112lg@users.noreply.github.com>
2024-02-29 10:28:43 +0000

    Update run.sh with commands

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd ArrowOS13
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --depth=1
+cd LineageOS21
+repo init -u https://github.com/accupara/los21.git -b lineage-21.0 --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
