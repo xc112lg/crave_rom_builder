ba07da5e15f6f1f50fa8dfef72d86ee3911abf74
xc112lg <xc112lg@users.noreply.github.com>
2024-02-29 10:42:33 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,19 +4,13 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch $\CLEAN_FLAG -- "
+crave run --no-patch --clean -- "
           #Thanks to UV n Team
           #© crave.io Inc. 2017-2024
         #no dual account just to build faster
               #respect the rule
 # 
 
-rm -rf .repo/local_manifests 
-git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
-              #respect the rule
-repo sync -c -j\$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
-lunch -userdebug
-m bacon
+rm -rf .repo/local_manifests && git clone https://github.com/LG-G6/scripts.git -b lineage-20 && mkdir .repo/local_manifests && cp scripts/roomservice.xml .repo/local_manifests/&& chmod +x scripts/sync.sh && . scripts/sync.sh && source build/envsetup.sh && lunch lineage_h872-userdebug && m -j16 bacon
 "
 . a.sh
