10b99073c4dc2592ab1e41b3d43ea47cad7cd35e
xc112lg <xc112lg@users.noreply.github.com>
2024-02-29 12:02:36 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -8,7 +8,13 @@ crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
-
-this is a special box for the big boys only dont use
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+rm -rf .repo/local_manifests what do delete i dont know
+git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
+              #respect the rule
+repo sync -c -j\$(nproc --all) --force-sync --no-clone-bundle --no-tags
+source build/envsetup.sh
+lunch lineage_h872-userdebug
+m bacon
 "
 . a.sh
