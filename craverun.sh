2ce1f29ff782c15a074fec89f8de61aa0c8723c5
xc112lg <xc112lg@users.noreply.github.com>
2024-02-29 20:46:03 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -4,17 +4,11 @@ git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/a.sh a.sh
 chmod +x a.sh
 basename "$(pwd)"> bp.txt
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
-repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
-rm -rf .repo/local_manifests what do delete i dont know
-git clone https://github.com/jayz1212/local_manifests -b main .repo/local_manifests
-              #respect the rule
-repo sync -c -j\$(nproc --all) --force-sync --no-clone-bundle --no-tags
-source build/envsetup.sh
-lunch lineage_h872-userdebug
-m bacon
+
+rm -rf .repo&& mkdir -p los&& cd los && rm -rf lineage_build_leaos .repo/local_manifests&& git clone https://github.com/xc112lg/lineage_build_leaos -b lineage-18.1&&repo init  --depth 1 -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs&& bash lineage_build_leaos/build.sh treble 64BVZ
 "
 . a.sh
