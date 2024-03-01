0b4deb9fabc3e4b61ccff721897ef6fe9a38a1fd
xc112lg <xc112lg@users.noreply.github.com>
2024-03-01 03:26:38 +0000

    Update run.sh with commands

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
 cd LineageOS21
-repo init -u https://github.com/accupara/los21.git -b lineage-21.0 --git-lfs --depth=1
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
