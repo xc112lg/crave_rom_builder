ef695e76c5cd14a9b443414ee537fb3ee75018d5
xc112lg <xc112lg@users.noreply.github.com>
2024-03-02 01:31:32 +0000

    Update run.sh with commands

--- run.sh
+++ run.sh
@@ -2,13 +2,9 @@
 export GH_TOKEN=$(cat gh_token.txt)
 gh auth login --with-token $GH_TOKEN
 gh auth status
-mkdir -p LineageOS21
+mkdir -p ArrowOS13
 bp=$(cat bp.txt)
 cp -f crave_rom_builder/run1.sh run1.sh
 chmod +x run1.sh
-cp -f crave_rom_builder/craverun.sh LineageOS21
-tmux set-environment bp "$bp"
-tmux kill-session -t $bp
-tmux new-session -d -s $bp
-tmux list-session -t $bp
-tmux send-keys -t $bp C-a C-k ./run1.sh C-m
+cp -f crave_rom_builder/craverun.sh ArrowOS13
+./run1.sh
