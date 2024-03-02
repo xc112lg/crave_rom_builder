fdcfa6a3e7d19d872cb974aadc480dddfd8e5ce0
xc112lg <xc112lg@users.noreply.github.com>
2024-03-02 20:00:44 +0000

    Update run.sh with commands

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 export GH_TOKEN=$(cat gh_token.txt)
 gh auth login --with-token $GH_TOKEN
 gh auth status
-mkdir -p LineageOS21
+mkdir -p ArrowOS13
 bp=$(cat bp.txt)
 cp -f crave_rom_builder/run1.sh run1.sh
 chmod +x run1.sh
-cp -f crave_rom_builder/craverun.sh LineageOS21
+cp -f crave_rom_builder/craverun.sh ArrowOS13
 ./run1.sh
