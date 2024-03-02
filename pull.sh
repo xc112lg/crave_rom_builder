b6f1c7ecd7582682ed93682612a0217e7e22e5df
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-02 22:19:59 +0800

    test

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm crave_rom_builder/*.zip crave_rom_builder/*.txt crave_rom_builder/*.json crave_rom_builder/*.img
-crave pull out/target/product/*/recovery.img
+#crave pull out/target/product/*/recovery.img
 #crave pull out/target/product/*/*.zip out/target/product/*/recovery.img out/target/product/*/*.json out/target/product/*/changelog_*.txt
 
 
