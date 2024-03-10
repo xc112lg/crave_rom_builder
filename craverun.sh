0dd2d723423c5ae3e45aa69d813180f6470e855c
xc112lg <xc112lg@users.noreply.github.com>
2024-03-10 23:02:57 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -3,7 +3,7 @@ rm -rf crave_rom_builder
 git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/pull.sh pull.sh
 chmod +x pull.sh
-crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
+crave run --no-patch --clean -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
