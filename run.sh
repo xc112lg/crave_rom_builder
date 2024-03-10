5717247eb5d4dba15cfd7700de06b481eb417301
xc112lg <xc112lg@users.noreply.github.com>
2024-03-10 04:42:26 +0000

    Update run.sh with commands

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GH_TOKEN=$(cat gh_token.txt)
-gh auth login --with-token $GH_TOKEN
-gh auth status
+#gh auth login --with-token $GH_TOKEN
+#gh auth status
 mkdir -p ArrowOS13
 bp=$(cat bp.txt)
 cp -f crave_rom_builder/run1.sh run1.sh
