5aab4c82d14eeae7e9b60ac19cda68fbc199516e
xc112lg <xc112lg@users.noreply.github.com>
2024-03-10 22:08:20 +0000

    Update run.sh with commands

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export GH_TOKEN=$(cat gh_token.txt)
 #gh auth login --with-token $GH_TOKEN
-#gh auth status
-mkdir -p ArrowOS13
+cd /crave-devspaces
+mkdir -p ArrowOS13-
 bp=$(cat bp.txt)
+rm -rf crave_rom_builder
+git clone https://$GH_TOKEN@github.com/xc112lg/crave_rom_builder.git
 cp -f crave_rom_builder/run1.sh run1.sh
 chmod +x run1.sh
-cp -f crave_rom_builder/craverun.sh ArrowOS13
+cp -f crave_rom_builder/craverun.sh ArrowOS13-
 ./run1.sh
--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd ArrowOS13
+cd ArrowOS13-
 repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --depth=1
 chmod +x craverun.sh
 . craverun.sh
