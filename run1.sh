f9bf2d7b393ead710fb0a9a608024637bfd2bde3
xc112lg <xc112lg@users.noreply.github.com>
2024-03-12 12:34:32 +0000

    Update run.sh with commands

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
-cd ArrowOS13-
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --depth=1
+cd CipherOS-
+repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen --git-lfs --depth=1
 chmod +x craverun.sh
 . craverun.sh
