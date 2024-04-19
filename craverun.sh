bf55860a1fc093c83e20f5666dcfa2179d5e71ec
xc112lg <xc112lg@users.noreply.github.com>
2024-04-19 05:35:49 +0000

    Update run.sh with commands

--- craverun.sh
+++ craverun.sh
@@ -7,6 +7,14 @@ crave run --no-patch  -- "          #© crave.io Inc. 2017-2024
               #Thanks to UV n Team
         #no dual account just to build faster
               #respect the rule
-mkdir justdoingtesting; ls
+
+rm -rf .repo/local_manifests 
+git clone https://github.com/xc112lg/local_manifests -b main .repo/local_manifests
+/opt/crave/resync.sh
+ 
+source build/envsetup.sh
+lunch lineage_h872-userdebug
+m installclean
+m bacon
 "
 . pull.sh
